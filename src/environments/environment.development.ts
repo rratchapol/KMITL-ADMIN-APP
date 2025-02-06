5d6b6acc30127130b6ae66a80116df1c82aea713
rratchapol <rratchapol@gmail.com>
2025-02-06 17:48:42 +0700

    แก้ logo website

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
     // baseURL: "https://trr.dev-asha.com"
-    baseURL: 'http://localhost:8000',
+    // baseURL: 'http://localhost:8000',
+    baseURL: 'http://ce67-17.cloud.ce.kmitl.ac.th',
+
 };
