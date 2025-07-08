ea576a0169d852f20c7571df4c2c2e6b500907a4
TAO <161469319+rratchapol@users.noreply.github.com>
2025-07-08 21:37:25 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     // baseURL: "http://localhost:9080/mlo-api/public"
     // baseURL: "https://trr.dev-asha.com"
-    baseURL: 'http://backend.kxchanges.online',
+    baseURL: 'http://backend.kxchanges.online:8000',
 };
