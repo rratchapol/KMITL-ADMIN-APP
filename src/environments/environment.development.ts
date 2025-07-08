563a3d9bbb3d4376f3f71f1f0a36755ec219e335
TAO <161469319+rratchapol@users.noreply.github.com>
2025-07-08 21:42:20 +0700

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // baseURL: "https://trr.dev-asha.com"
     // baseURL: 'http://localhost:8000',
-    baseURL: 'http://backend.kxchanges.online',
+    baseURL: 'http://backend.kxchanges.online:8000',
 
 };
